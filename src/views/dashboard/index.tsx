3af5f47e1780c193bd314c6b6c43adf8fe4cedf6
luyiyuan <i@luyiyuan.me>
2024-05-10 21:15:55 +0800

    chapter: 9.2 Echarts 图标开发入门

--- src/views/dashboard/index.tsx
+++ src/views/dashboard/index.tsx
@@ -1,7 +1,42 @@
-import {Descriptions} from 'antd'
+import {Descriptions, Card, Button} from 'antd'
 import styles from './index.module.less'
+import * as echarts from 'echarts'
+import {useEffect} from 'react'
 
 export default function DashBoard() {
+  useEffect(() => {
+    const lineChartDom = document.getElementById('lineChart')
+    const chartInstance = echarts.init(lineChartDom as HTMLElement)
+    chartInstance.setOption({
+      tooltip: {
+        trigger: 'axis'
+      },
+      grid: {
+        left: 50,
+        right: 50,
+        bottom: 20
+      },
+      xAxis: {
+        data: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月']
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [
+        {
+          name: '订单',
+          type: 'line',
+          data: [10, 20, 30, 50, 60, 70, 80, 90, 100, 110, 120, 120]
+        },
+        {
+          name: '流水',
+          type: 'line',
+          data: [1000, 2000, 3000, 5000, 600, 800, 2000, 3200, 1100, 1200, 6000, 5000]
+        }
+      ]
+    })
+  }, [])
+
   return (
     <div className={styles.dashboard}>
       <div className={styles.userInfo}>
@@ -35,6 +70,23 @@ export default function DashBoard() {
           <div className="title">开通城市</div>
           <div className={styles.data}>50座</div>
         </div>
+      </div>
+
+      <div className={styles.chart}>
+        <Card title='订单和流水走势图' extra={<Button type='primary'>刷新</Button>}>
+          <div id="lineChart" className={styles.itemChart}></div>
+        </Card>
+      </div>
+      <div className={styles.chart}>
+        <Card title='司机分布' extra={<Button type='primary'>刷新</Button>}>
+          <div id="pieChartCity" className={styles.itemChart}></div>
+          <div id="pieChartAge" className={styles.itemChart}></div>
+        </Card>
+      </div>
+      <div className={styles.chart}>
+        <Card title='模型诊断' extra={<Button type='primary'>刷新</Button>}>
+          <div id="radarChart" className={styles.itemChart}></div>
+        </Card>
       </div>
     </div>
   )
